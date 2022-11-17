24d207737bee409d8ae5ee90c8c5e7fea7e5ba2f
Jay <jayswinney28@yahoo.com>
2022-11-17 10:56:38 -0500

    Create calc.js

---
+++ calc.js
@@ -0,0 +1,10 @@
+
+// store input numbers
+const num1 = parseInt(prompt('Enter the first number '));
+const num2 = parseInt(prompt('Enter the second number '));
+
+//add two numbers
+const sum = (num1/num2)*100;
+
+// display the sum
+alert(`Your win percentage is ${sum}`);
